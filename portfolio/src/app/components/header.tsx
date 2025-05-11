dacd64a94d73eb5a93bc1ef8dd11985a048671e6
prahalad.an <prahaladanand6@gmail.com>
2025-05-11 18:22:44 -0500

    text change

--- portfolio/src/app/components/header.tsx
+++ portfolio/src/app/components/header.tsx
@@ -11,7 +11,7 @@ export default function Header() {
       <div className={styles.contactContainer}>
         <h2 id="contact"><a onClick={copyToClipboard} title="Email">prahaladanand6@gmail.com</a> | <a href="https://www.linkedin.com/in/prahalad-anand-524636297/" title="LinkedIn" target="__blank">LinkedIn</a> | <a href="https://github.com/PPilot2" title="Github" target="__blank">Github</a> | <a href="resume5.10.25.pdf" title="Resume" target="__blank">Resume</a></h2>
       </div>
-      <h3>AI Intern at <a>Splenta Systems</a></h3>
+      <h3>Web Developer and AI Researcher</h3>
     </div>
   );
 }
