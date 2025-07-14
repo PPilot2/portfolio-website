71d974681a62ddbb2f12cab04b1aa901a04b1cf6
prahalad.an <prahaladanand6@gmail.com>
2025-07-14 17:41:01 -0500

    new projects

--- portfolio/src/app/components/Projects.tsx
+++ portfolio/src/app/components/Projects.tsx
@@ -5,14 +5,22 @@ export default function Projects() {
   const projectsData = [
     {
       id: 1,
-      title: "K-shot-GAN-synthetic-data",
-      image: "/kshot.png",
-      description: "Improving the generation of synthetic data in LLMs using k-shot prompting and a GAN.",
-      tags: ["Tensorflow", "PyTorch", "Pandas"],
-      sourceUrl: "https://github.com/PPilot2/k_shot-GAN-synthetic-data"
+      title: "Collaborative Code Editor",
+      image: "/codeEditor.png",
+      description: "A web-based, multi-file code editor with live code execution, supporting multiple programming languages.",
+      tags: ["Next.js", "TypeScript", "CSS"],
+      sourceUrl: "https://github.com/PPilot2/collaborative-code-editor"
     },
     {
       id: 2,
+      title: "Portfolio Website",
+      image: "/portfolio-pic.png",
+      description: "A portfolio website built with Next.js and TypeScript.",
+      tags: ["Next.js", "TypeScript", "CSS"],
+      sourceUrl: "https://github.com/PPilot2/portfolio-website"
+    },
+    {
+      id: 3,
       title: "JSON, CSV, and XML Data Generation",
       image: "/json-vis.png",
       description: "Visualizes JSON, XML, and CSV data into beautiful flowcharts, trees, and data tables using GoJS.",
@@ -20,7 +28,15 @@ export default function Projects() {
       sourceUrl: "https://github.com/PPilot2/JSON-Visualizer"
     },
     {
-      id: 3,
+      id: 4,
+      title: "K-shot-GAN-synthetic-data",
+      image: "/kshot.png",
+      description: "Improving the generation of synthetic data in LLMs using k-shot prompting and a GAN.",
+      tags: ["Tensorflow", "PyTorch", "Pandas"],
+      sourceUrl: "https://github.com/PPilot2/k_shot-GAN-synthetic-data"
+    },
+    {
+      id: 5,
       title: "Penicillium-detection-in-citrus-sinensis",
       image: "/penicillium.png",
       description: "Improving the detection of penicillium in citrus sinensis with a CNN.",
@@ -28,21 +44,13 @@ export default function Projects() {
       sourceUrl: "https://github.com/PPilot2/Penicillium-detection-in-citrus-sinensis"
     },
     {
-      id: 4,
+      id: 6,
       title: "Dev Extension",
       image: "/dev-extension.png",
       description: "A Chrome extension which links to Github accounts in order to display prs and keep up with the latest trends.",
       tags: ["HTML", "CSS", "JavaScript"],
       sourceUrl: "https://github.com/PPilot2/dev-extension"
     },
-    {
-      id: 5,
-      title: "Portfolio Website",
-      image: "/portfolio-pic.png",
-      description: "A portfolio website built with Next.js and TypeScript.",
-      tags: ["Next.js", "TypeScript", "CSS"],
-      sourceUrl: "https://github.com/PPilot2/portfolio-website"
-    }
   ];
 
   return (
--- portfolio/src/app/components/footer.tsx
+++ portfolio/src/app/components/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className={styles.footer}>
       <div className={styles.footerContent}>
-        <p>Made with ❤️ by <a href="https://github.com/PPilot2" className={styles.footerLink} target="__blank">Prahalad Anand</a></p>
+        <p>Made with ❤️ by <a href="https://github.com/PPilot2/portfolio-website" className={styles.footerLink} target="__blank">Prahalad Anand</a></p>
       </div>
     </footer>
   );
