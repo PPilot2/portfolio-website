cb598c11ee95730bb12ac280632a7461a086736f
prahalad.an <prahaladanand6@gmail.com>
2025-07-31 17:18:20 -0500

    styling changes

--- portfolio/next.config.ts
+++ portfolio/next.config.ts
@@ -1,23 +1,7 @@
 import type { NextConfig } from "next";
-import mdx from '@next/mdx';
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
-
-const withMDX = mdx({
-  extension: /\.mdx?$/,
-  options: {
-    // If you use remark-gfm, install it and uncomment:
-    // remarkPlugins: [require('remark-gfm')],
-    // If you use rehype plugins, install them and uncomment:
-    // rehypePlugins: [/* ... */],
-  },
-})
-
-module.exports = withMDX({
-  // Support both .md and .mdx files
-  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-})
 
 export default nextConfig;
