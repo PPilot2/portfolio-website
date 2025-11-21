18f50507eb552e67660fa44fb0cef18f5ebbd700
prahalad.an <prahaladanand6@gmail.com>
2025-11-21 14:14:46 -0600

    responsive

--- portfolio/src/app/components/header.tsx
+++ portfolio/src/app/components/header.tsx
@@ -1,6 +1,6 @@
 "use client";
 import styles from "../page.module.css";
-import Link from "next/link";
+// import Link from "next/link";
 import { useEffect, useState } from "react";
 import { CenterAlert } from "./alertbox";
 
